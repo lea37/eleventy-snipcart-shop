56744b0a8de476cb4dd51a960b6d385589c3970a
Lea Tortay <leatortay@gmail.com>
2020-03-28 09:30:38 +0100

    responsive

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const moment = require("moment");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
     // copy assets folder
@@ -6,14 +6,18 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({'src/_includes/assets/scripts/main.min.js': 'scripts/main.min.js'});
     eleventyConfig.addPassthroughCopy({'src/_includes/assets/images/': 'images/'});
 
-    eleventyConfig.addNunjucksFilter("date", function() {
-        moment().locale('fr');
-      return moment().format('DD/MM/YYYY');
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
     });
 
     // posts collection
     eleventyConfig.addCollection("posts", function(collection) {
-      return collection.getFilteredByGlob("./src/posts/*.md").reverse();
+        return collection.getFilteredByGlob("./src/posts/*.md").reverse();
     });
 
     // Base config
--- src/posts/bienvenue.md
+++ src/posts/bienvenue.md
@@ -1,7 +1,7 @@
 ---
 title: Mon premier article
 description: Mon premier article
-date: 2020-03-27
+date: 2020-03-16
 layout: layouts/post.njk
 ---
 
